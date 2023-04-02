814e600b4daebeea6a54fa4d8297c9a1c7e98e70
jdlms <lamorey.joshua@gmail.com>
2023-04-02 11:04:38 +0200

    buliding collection

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,33 +1,74 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import { ModalDetails } from "@/components/ModalDetails";
 import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { Dog } from "@/interfaces/dog";
 import { player } from "@/lib/player";
-import { getCollection } from "./api/getCollection";
-import { useEffect } from "react";
+import Image from "next/image";
+import { useState } from "react";
+import { ScaleLoader } from "react-spinners";
 
 export default function Collection() {
   const [playerData, setPlayerData] = useLocalStorage("guess-that-dog", player);
+  const [modalText, setModalText] = useState({});
+  const [isModalOpen, setisModalOpen] = useState(false);
 
-  let ids = playerData.correctBreedIds;
-  console.log(ids);
+  let collection = playerData.correctBreedIds;
 
-  useEffect(() => {
-    let isSubscribed = true;
-    if (isSubscribed) {
-      getDogs();
-    }
-    return () => {
-      isSubscribed = false;
-    };
-  }, []);
+  const handleClick = (dog: Dog) => {};
 
-  const getDogs = async function () {
-    try {
-      const res = await getCollection(ids);
-      console.log(res);
-    } catch (error) {
-      console.error("There was an error:", error);
-    }
-  };
-
-  return <></>;
+  return (
+    <>
+      {!isModalOpen ? (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <ul
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              flexWrap: "wrap",
+              gap: "10px",
+              listStyle: "none",
+            }}
+          >
+            {collection.length > 0 ? (
+              collection.map((dog: Dog) => {
+                return (
+                  <li key={dog.id}>
+                    <Image
+                      className="dogPic"
+                      onClick={() => handleClick(dog)}
+                      style={{
+                        // height: "150px",
+                        objectFit: "fill",
+                        borderWidth: "2px",
+                        borderColor: "white",
+                        borderStyle: "solid",
+                        cursor: "pointer",
+                      }}
+                      src={dog.url}
+                      alt={dog.breeds[0].name}
+                      height={150}
+                      width={200}
+                    />
+                  </li>
+                );
+              })
+            ) : (
+              <ScaleLoader color="#ffffff" />
+            )}
+          </ul>
+        </div>
+      ) : (
+        <>
+          <ModalDetails
+            isModalOpen={isModalOpen}
+            setIsModalOpen={setisModalOpen}
+            modalText={modalText}
+            // isGuessCorrect={isGuessCorrect}
+            // setIsGuessCorrect={setIsGuessCorrect}
+          />
+        </>
+      )}
+    </>
+  );
 }
